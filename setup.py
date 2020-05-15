4f3add86040758df90ee924d5d77b7a42574e247
Mustafa Baser <mbaser@mail.com>
2020-05-15 22:17:11 +0300

    tui: add scim server installation option

--- setup.py
+++ setup.py
@@ -2588,7 +2588,7 @@ class Setup(object):
             self.install_oxtrust()
 
         if self.installScimServer:
-            self.pbar.progress("oxtrust", "Installing Gluu components: Scim Server", False)
+            self.pbar.progress("scim", "Installing Gluu components: Scim Server", False)
             self.install_scim_server()
 
         if self.installSaml:
@@ -5814,6 +5814,7 @@ if __name__ == '__main__':
         msg.installation_step_number = 33
         
         msg.os_type = installObject.os_type
+        msg.os_version = installObject.os_version
         msg.os_initdaemon = installObject.os_initdaemon
         msg.apache_version = installObject.apache_version
         msg.current_mem_size = current_mem_size
